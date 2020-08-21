74c6fb4735901071f0cdaa9169b585b781b21f8f
hinal-025 <68282568+hinal-025@users.noreply.github.com>
2020-08-21 09:51:04 +0530

    Add files via upload

---
+++ C_numbers.c
@@ -0,0 +1,24 @@
+/*C program of numbers 0 and 1 using switch statement */
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+    system("cls");
+    int n;
+    printf("\nEnter number :");
+    scanf("%d",&n);
+    switch(n)
+    {
+    case 0:
+        printf("\nnumber is 0");
+        break;
+    case 1:
+        printf("\nnumber is 1 ");
+        break;
+    default:
+        printf("\nthe entered number isn`t in the list\n");
+        break;
+    }
+    system("pause");
+	return 10;
+}
